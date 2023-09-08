015cc7979bdad7c3dd17fde90c85236fffd46629
Leidy Londoño <leidy_londono23212@elpoli.edu.co>
2023-09-07 20:46:58 -0500

    Inicio de sección 15 CRUD

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -8,9 +11,44 @@ import { Component } from '@angular/core';
 })
 export class NewPageComponent {
 
+  public heroForm = new FormGroup({
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('',{nonNullable:true}),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
+
   public publishers = [
     {id: 'DC Comics', desc: 'DC - Comics'},
     {id: 'Marvel Comics', desc: 'Marvel - Comics'},
-  ]
+  ];
+
+  constructor(private heroesService: HeroesService){}
+
+  get currentHero(): Hero{
+    const hero = this.heroForm.value as Hero;
+
+    return hero;
+  }
+
+  onSubmit(): void{
+    if(this.heroForm.invalid) return;
+
+    if(this.currentHero.id){
+      this.heroesService.updateHero(this.currentHero)
+        .subscribe(hero=>{
+          //TODO: mostrar snackbar
+        });
+        return;
+    }
+    this.heroesService.addHero(this.currentHero)
+      .subscribe(hero=>{
+        //TODO: mostrar snackbar, y navegar a /heroes/edit/ hero.id
+      });
+
+  }
 
 }
